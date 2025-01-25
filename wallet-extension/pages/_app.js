067f97ec348adfeed727ddf39e212b7b7c268a76
derek2403 <derekliew0@gmail.com>
2025-01-25 23:00:58 +0800

    added assets and script files

--- wallet-extension/pages/_app.js
+++ wallet-extension/pages/_app.js
@@ -1,7 +1,14 @@
 import "@/styles/globals.css";
 import { NextUIProvider } from "@nextui-org/react";
+import { useSSR } from '@nextui-org/react'
 
 export default function App({ Component, pageProps }) {
+  const { isBrowser } = useSSR()
+
+  if (!isBrowser) {
+    return null;
+  }
+
   return (
     // Adding a max width and height for extension popup
     <div className="w-[400px] h-[600px] overflow-auto">
